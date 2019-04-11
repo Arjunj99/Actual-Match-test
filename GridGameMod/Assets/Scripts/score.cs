a7923a522a68205d8dbc96150bdfe8024016c794
Arjunj99 <asj397@nyu.edu>
2019-04-11 16:23:37 -0400

    Committing Game Code

---
+++ GridGameMod/Assets/Scripts/score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+    Text txt;
+    // Start is called before the first frame update
+    void Start() {
+        txt = gameObject.GetComponent<Text>();
+        txt.text = "Score: " + GameManager.instance.score;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        txt.text = "Score: " + GameManager.instance.score;
+    }
+}
